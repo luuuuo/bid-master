c93892ffbd0b83d705cf79f9ebffa5d02b560114
xiaopeng <2584730947@qq.com>
2023-07-20 10:37:27 +0800

    refactor: review pro4

--- contracts/pro/pro4/AuctionFactory.sol
+++ contracts/pro/pro4/AuctionFactory.sol
@@ -11,20 +11,64 @@ contract AuctionFactory {
     }
     mapping(address => Auctions[]) public userAuctions; 
 
-    function createAuctions(AuctionType _auctionType, address _newImplementation) external returns (address auctionAddress) {
+    address public blindAuctionImpl;
+    address public openAuctionImpl;
+
+    modifier onlyOwner() {
+        if(getOwnerAddress() != address(0))
+            require(getOwnerAddress() == msg.sender, "not owner's operation");
+        _;
+    }
+
+    constructor(address _blindAuctionImpl, address _openAuctionImpl) {
+        blindAuctionImpl = _blindAuctionImpl;
+        openAuctionImpl = _openAuctionImpl;
+        setOwnership(msg.sender);
+    }
+
+    function createAuctions(AuctionType _auctionType) external returns (address auctionAddress) {
         // 创建新合约
         if(_auctionType == AuctionType.Blind){
             BlindAuction blindAuction = new BlindAuction();
-            blindAuction.upgradeTo(_newImplementation);
+            blindAuction.upgradeTo(blindAuctionImpl);
             blindAuction.setOwnership(msg.sender);
             auctionAddress = address(blindAuction); 
         }else{
             OpenAuction openAuction = new OpenAuction();
-            openAuction.upgradeTo(_newImplementation);
+            openAuction.upgradeTo(openAuctionImpl);
             openAuction.setOwnership(msg.sender);
             auctionAddress = address(openAuction);
         }
         userAuctions[msg.sender].push(Auctions(_auctionType, auctionAddress));
         return auctionAddress;
+    }
+
+    function setNewBlindAuctionImpl(address _newBlindAuctionImpl) external onlyOwner{
+        require(_newBlindAuctionImpl != address(0),"The new BlindAuctionImpl address cannot be 0x00");
+        blindAuctionImpl = _newBlindAuctionImpl;
+    }
+
+    function setNewOpenAuctionImpl(address _newOpenAuctionImpl) external onlyOwner{
+        require(_newOpenAuctionImpl != address(0),"The new OpenAuctionImpl address cannot be 0x00");
+        openAuctionImpl = _newOpenAuctionImpl;
+    }
+
+    function getOwnerPosition() public virtual pure returns(bytes32 ownerPosition){
+        ownerPosition = keccak256("auction-factory-owner");
+    }
+    
+    function getOwnerAddress() public view returns(address ownerAddress) {
+        bytes32 position = getOwnerPosition();
+        assembly {
+            ownerAddress := sload(position)
+        }
+    }
+
+    function setOwnership(address _newOwner) public onlyOwner{
+        require(_newOwner != address(0));
+        bytes32 newOwnerPosition = getOwnerPosition();
+        assembly {
+            sstore(newOwnerPosition, _newOwner)
+        }
     }
 }
