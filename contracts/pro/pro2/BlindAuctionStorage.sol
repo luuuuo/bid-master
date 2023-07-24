a70b50fd85ca185036fb72cda3b2b761227e3375
luuuuo <forestandocean@126.com>
2023-07-24 20:26:53 +0800

    feat: remove unuse param

--- contracts/pro/pro2/BlindAuctionStorage.sol
+++ contracts/pro/pro2/BlindAuctionStorage.sol
@@ -13,11 +13,4 @@ contract BlindAuctionStorage {
     address payable public beneficiary;
 
     bool internal initStatus = false;
-
-    /**
-     * @dev This empty reserved space is put in place to allow future versions to add new
-     * variables without shifting down storage in the inheritance chain.
-     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-     */
-    uint256[45] private __gap;
 }
--- contracts/pro/pro4/storages/AuctionStorage.sol
+++ contracts/pro/pro4/storages/AuctionStorage.sol
@@ -10,11 +10,5 @@ contract AuctionStorage {
     uint public auctionEndTime;
     address public highestBidder;
     uint public highestBid;
-    /**
-     * @dev This empty reserved space is put in place to allow future versions to add new
-     * variables without shifting down storage in the inheritance chain.
-     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-     */
-    uint256[45] private __gap;
 
 }
